d73de0687fd60d1dac4b07595374835a626dac3f
xinyu275 <xinyu257@126.com>
2018-07-30 12:29:29 +0800

    sublime
    sublime

---
+++ route.go
@@ -0,0 +1,217 @@
+package webapp
+
+import (
+	"net/http"
+)
+
+type Handle func(http.ResponseWriter, *http.Request, Params)
+
+type Param struct {
+	Key   string
+	Value string
+}
+type Params []Param
+
+func (ps Params) ByName(name string) string {
+	for i, v := range ps {
+		if v.Key == name {
+			return v.Value
+		}
+	}
+
+	return ""
+}
+
+type Router struct {
+	trees map[string]*node
+
+	RedirectTrailingSlash  bool
+	RedirectFixedPath      bool
+	HandleMethodNotAllowed bool
+	HandleOPTIONS          bool
+	NotFound               http.Handler
+	MethodNotAllowed       http.Handler
+	PanicHandler           func(http.ResponseWriter, *http.Request, interface{})
+}
+
+var _ http.Handler = New()
+
+func New() *Router {
+	return &Router{
+		RedirectTrailingSlash:  true,
+		RedirectFixedPath:      true,
+		HandleMethodNotAllowed: true,
+		HandleOPTIONS:          true,
+	}
+}
+
+func (r *Router) GET(path string, handle Handle) {
+	r.Handle("GET", path, handle)
+}
+func (r *Router) HEAD(path string, handle Handle) {
+	r.Handle("HEAD", path, handle)
+}
+func (r *Router) OPTIONS(path string, handle Handle) {
+	r.Handle("OPTIONS", path, handle)
+}
+func (r *Router) POST(path string, handle Handle) {
+	r.Handle("POST", path, handle)
+}
+func (r *Router) PUT(path string, handle Handle) {
+	r.Handle("PUT", path, handle)
+}
+func (r *Router) PATCH(path string, handle Handle) {
+	r.Handle("PATCH", path, handle)
+}
+func (r *Router) DELETE(path string, handle Handle) {
+	r.Handle("DELETE", path, handle)
+}
+
+func (r *Router) Handle(method, path string, handle Handle) {
+	if path[0] != '/' {
+		panic("path must begin with '/' in path '" + path + "'")
+	}
+
+	if r.trees == nil {
+		r.trees = make(map[string]*node)
+	}
+
+	root := r.trees[method]
+	if root == nil {
+		root = new(node)
+		r.trees[method] = root
+	}
+	root.addRoute(path, handle)
+}
+
+func (r *Router) HandleFunc(method, path string, handler http.HandlerFunc) {
+	r.Handle(method, path, handler)
+}
+
+func (r *Router) ServerFiles(path string, root http.FileSystem) {
+	if len(path) < 10 || path[len(path)-10:] != "/*filepath" {
+		panic("path must end with /*filepath in path '" + path + "'")
+	}
+
+	fileServer := http.FileServer(root)
+
+	r.GET(path, func(w http.ResponseWriter, req *http.Request, ps Params) {
+		req.URL.Path = ps.ByName("filepath")
+		fileServer.ServeHTTP(w, req)
+	})
+}
+
+func (r *Router) recv(w http.ResponseWriter, req *http.Request) {
+	if rcv := recover(); rcv != nil {
+		r.PanicHandler(w, req, rcv)
+	}
+}
+
+func (r *Router) Lookup(method, path string) (Handle, Params, bool) {
+	if root := r.trees[method]; root != nil {
+		return root.getValue(path)
+	}
+	return nil, nil, false
+}
+
+func (e *Router) allowed(path, reqMethod string) (allow string) {
+	if path == "*" {
+		for method := range r.trees {
+			if method == "OPTIONS" {
+				continue
+			}
+
+			if len(allow) == 0 {
+				allow = method
+			} else {
+				allow += "," + method
+			}
+		}
+	} else {
+		for method := range r.trees {
+			if method == reqMethod || method == "OPTIONS" {
+				continue
+			}
+
+			handle, _, _ := r.trees[method].getValue(path)
+			if handle != nil {
+				if len(allow) == 0 {
+					allow = method
+				} else {
+					allow += "," + method
+				}
+			}
+		}
+	}
+
+	if len(allow) > 0 {
+		allow += ",OPTIONS"
+	}
+
+	return
+}
+
+func (r *Router) ServerHTTP(w http.ResponseWriter, req *http.Request) {
+	if r.PanicHandler != nil {
+		defer r.recv(w, req)
+	}
+
+	path := req.URL.Path
+
+	if root := r.trees[req.Method]; root != nil {
+		if handle, ps, tsr := root.getValue(path); handle != nil {
+			handle(w, req, ps)
+			return
+		} else if req.Method != "CONNECT" && path != "/" {
+			code := 301
+			if req.Method != "GET" {
+				code = 307
+			}
+
+			if tsr && r.RedirectTrailingSlash {
+				if len(path) > 1 && path[len(path)-1] == '/' {
+					req.URL.Path = path[:len(path)-1]
+				} else {
+					req.URL.Path = path + "/"
+				}
+				http.Redirect(w, req, req.URL.String(), code)
+				return
+			}
+
+			if r.RedirectFixedPath {
+				fixedPath, found := root.findCaseInsensitivePath(
+					CleanPath(path), r.RedirectTrailingSlash)
+				if found {
+					req.URL.Path = string(fixedPath)
+					http.Redirect(w, req, req.URL.String(), code)
+					return
+				}
+			}
+		}
+	}
+	if req.Method == "OPTIONS" && r.HandleOPTIONS {
+		if allow := r.allowed(path, req.Method); len(allow) > 0 {
+			w.Header().Set("Allow", allow)
+			return
+		}
+	} else {
+		if r.HandleMethodNotAllowed {
+			if allow := r.allowed(path, req.Method); len(allow) > 0 {
+				w.Header().Set("Allow", allow)
+				if r.MethodNotAllowed != nil {
+					r.MethodNotAllowed.ServeHTTP(w, req)
+				} else {
+					http.Error(w, http.StatusText(http.StatusMethodNotAllowed),
+						http.StatusMethodNotAllowed)
+				}
+				return
+			}
+		}
+	}
+
+	if r.NotFound != nil {
+		r.NotFound.ServeHTTP(w, req)
+	} else {
+		http.NotFound(w, req)
+	}
+}
